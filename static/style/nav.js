d37c0fb220caf5aabb4d67fbb5d60eae2c9d0138
Lauren Knight <lknight2@mail.umw.edu>
2022-12-06 18:55:53 -0500

    home and login done

---
+++ static/style/nav.js
@@ -0,0 +1,5 @@
+$(function() {
+    $("#menu-button").click(function() {
+        $("header > ul").toggleClass("visible-flex");
+    });
+});
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
+app.loggedin = False
+
 @app.route('/')
 def home():
-    return "hello world"
+    if not app.loggedin:
+        return redirect(url_for('login'))
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if app.loggedin:
+        return redirect(url_for('home'))
+    
+    if request.method == 'POST':
+        app.loggedin = True
+        return redirect(url_for('home'))
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return "TBI"
+
+@app.route('/logout')
+def logout():
+    app.loggedin = False
+    return redirect(url_for('login'))   
+
+@app.route('/account')
+def account():
+    if not app.loggedin:
+        return redirect(url_for('login'))
+    return "TBI"
