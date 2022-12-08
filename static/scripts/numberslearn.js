22a297dbc1c44d4d724d9fece6fbf949155e5a03
Lauren Knight <lknight2@mail.umw.edu>
2022-12-07 23:02:03 -0500

    attempt to fix animation

--- static/scripts/numberslearn.js
+++ static/scripts/numberslearn.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('#you-try').change(function() {
+    $('#you-try').input(function() {
         let value = $(this).val();
         let placeholder = $(this).attr("placeholder");
         let disable = value != placeholder;
