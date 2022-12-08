ce8c1b0b67f0943a5d78c152c4955d6e78972a4a
Lauren Knight <lknight2@mail.umw.edu>
2022-12-07 23:05:00 -0500

    fix input event

--- static/scripts/numberslearn.js
+++ static/scripts/numberslearn.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('#you-try').input(function() {
+    $('#you-try').on('input', function() {
         let value = $(this).val();
         let placeholder = $(this).attr("placeholder");
         let disable = value != placeholder;
