7cbeec94cde0549972e6834f8f3022a32b4b205c
Lauren Knight <lknight2@mail.umw.edu>
2023-02-09 16:11:32 -0500

    fix speech url

--- static/scripts/vocabpractice.js
+++ static/scripts/vocabpractice.js
@@ -229,7 +229,7 @@ function speak() {
 }
 
 function generateSoundURL() {
-    return '/vocab/term/speech/' + currentTerm.id;
+    return '/vocab/term/' + currentTerm.id + '/speech';
 }
 
 $(function() {
