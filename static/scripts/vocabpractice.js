29642decfa93176a786f59a677258cee645c547d
Lauren Knight <lknight2@mail.umw.edu>
2023-02-13 08:02:12 -0500

    attempt to fix apostrophe issue

--- static/scripts/vocabpractice.js
+++ static/scripts/vocabpractice.js
@@ -78,7 +78,7 @@ function chooseAndDisplayNextPrompt() {
 
 function userSubmittedAnswer() {
     if (state == STATE_PROMPT) {
-        let input = userInput.val().toLowerCase().trim();
+        let input = userInput.val().toLowerCase().trim().replaceAll('`', "'").replaceAll('’', "'");
         let correct;
         if (mode == MODE_TRANSLATE_TO_ENGLISH) {
             correct = input == currentTerm.english.toLowerCase();
