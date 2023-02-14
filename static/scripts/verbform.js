f98b7569d7176cc7ca2f19e57b59dee32ae98949
Lauren Knight <lknight2@mail.umw.edu>
2023-02-14 13:20:38 -0500

    attempt to fix script issue

--- static/scripts/verbform.js
+++ static/scripts/verbform.js
@@ -14,10 +14,10 @@ $(function() {
         [ '#vous-ftr', 'ez'],
         [ '#ils-ftr', 'ont']
     ];
-    $('#conditional-future-stem').change(function() {
+    $('#conditional-future-stem').on("input", function() {
         let value = $(this).val();
         for (let pair in idsAndEndings) {
-            $(pair[0]).html(pair[1]);
+            $(pair[0]).html(value + pair[1]);
         }
     });
 });
