5272ca046a7c4a020fc30cf956f1185c9927a662
Matt Nguyen <92770341+mnnguyen4@users.noreply.github.com>
2023-03-06 15:07:55 -0500

    Get ID from URL
    The Event Page now checks if the url contains the Event ID. If so, the top header and the title of the page displays the ID. Else, a default value "No Event ID" is placed. Future plans include redirecting the user to the calendar view if there is no specified ID.

--- eventView.php
+++ eventView.php
@@ -5,24 +5,50 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<?php require_once('universal.inc') ?>
-		<title>Event View</title>	
+		<?php
+      require_once('universal.inc');
+
+      // check if user has reached this page
+      // with an event ID
+      if (isset($_GET["event"])) {
+        echo '<title>'.$_GET["event"].' - Event View</title>';
+      } else {
+        // TODO: If the event id is not in the url,
+        // then the user will be redirected to
+        // the calendar view
+		    echo '<title>No Event ID - Event View</title>';
+      }
+    ?>
 	</head>
 	
 	<body>
-		<?php require_once('header.php') ?>
-		<h1>Event Name: </h1>
-		<p>Event Date(s): </p>
-		<p>Event Start Time: </p>
-		<p>Event End Time: </p>
-		<p>Event Location: </p>
-		<p>Event Description: </p>
+		<?php require_once('header.php');
+
+    // check if user has reached this page
+    // with an event ID
+    if (isset($_GET["event"])) {
+		  echo '<h1>'.$_GET["event"].' - Event View: </h1>';
+    } else {
+      echo '<h1>No Event ID - Event View:</h1>';
+    }
+    ?>
+		<p>Event Date(s): March 6th, 2023</p>
+		<p>Event Start Time: 2:00pm</p>
+		<p>Event End Time: 5:00pm</p>
+		<p>Event Location: 1234 Drive Dr.</p>
+		<p>Event Description: This is a description</p>
 		<br>
 	
 		<body>
 			<h1>List of Volunteers for Event</h1>	
+      <li>
+        <ul>&nbsp;Joe</ul>
+        <ul>&nbsp;Alice</ul>
+        <ul>&nbsp;Bob</ul>
+        <ul>&nbsp;Michelle</ul>
+      </li>
 		</body>
-		
+    <br>
 		<body>
 			<h1>Access/Insert Training Materials</h1>	
 		</body>
