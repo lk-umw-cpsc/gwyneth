954461edf68bdbce9ff0fd360d674b6bfebcf7a9
Matt Nguyen <92770341+mnnguyen4@users.noreply.github.com>
2023-03-08 10:04:50 -0500

    Change UI
    Changed most <h1> tags to <h2> tags to keep UI consistent with rest of website. Centered <h2> tags and information.

--- event.php
+++ event.php
@@ -11,13 +11,13 @@
       // check if user has reached this page
       // with an event ID
       if (isset($_GET["id"])) {
-        echo '<title>'.$_GET["id"].' - Event View</title>';
+        echo '<title>'.$_GET["id"].' - Event</title>';
       } else {
         header('Location: calendar.php');
         // TODO: If the event id is not in the url,
         // then the user will be redirected to
         // the calendar view
-		    echo '<title>No Event ID - Event View</title>';
+		    echo '<title>No Event ID - Event</title>';
       }
     ?>
 	</head>
@@ -28,34 +28,50 @@
     // check if user has reached this page
     // with an event ID
     if (isset($_GET["id"])) {
-		  echo '<h1>'.$_GET["id"].' - Event View: </h1>';
+		  echo '<h1>'.$_GET["id"].' - Event: </h1>';
     } else {
-      echo '<h1>No Event ID - Event View:</h1>';
+      echo '<h1>No Event ID - Event:</h1>';
     }
     ?>
-		<p>Event Date(s): March 6th, 2023</p>
-		<p>Event Start Time: 2:00pm</p>
-		<p>Event End Time: 5:00pm</p>
-		<p>Event Location: 1234 Drive Dr.</p>
-		<p>Event Description: This is a description</p>
+    <!-- TODO: will figure out another way to center
+               later -->
+		<p><center>Event Date(s): March 6th, 2023</center></p>
+		<p><center>Event Start Time: 2:00pm</center></p>
+		<p><center>Event End Time: 5:00pm</center></p>
+		<p><center>Event Location: 1234 Drive Dr.</center></p>
+		<p><center>Event Description: This is a description</center></p>
 		<br>
 	
 		<body>
-			<h1>List of Volunteers for Event</h1>	
-      <li>
-        <ul>&nbsp;Joe</ul>
-        <ul>&nbsp;Alice</ul>
-        <ul>&nbsp;Bob</ul>
-        <ul>&nbsp;Michelle</ul>
-      </li>
+			<h2>
+        <center>List of Volunteers for Event</center>
+      </h2>
+      <!-- TODO: will figure out another way to center
+                 later -->
+        <li>
+            <ul><center>&nbsp;Joe</center></ul>
+            <ul><center>&nbsp;Alice</center></ul>
+            <ul><center>&nbsp;Bob</center></ul>
+            <ul><center>&nbsp;Michelle</center></ul>
+       </li>
 		</body>
     <br>
 		<body>
-			<h1>Access/Insert Training Materials</h1>	
+			<h2>
+        <center>Access/Insert Training Materials</center>
+      </h2>	
+      <center>
+        To be added
+      </center>
 		</body>
 		
+    <!-- temporary breaks to separate headers -->
+    <br><br>
+
 		<body>
-			<h1>Post-Event Media</h1>	
+			<h2>
+        <center>Post-Event Media</center>
+      </h2>	
 		</body>
 		
 		<center>		
