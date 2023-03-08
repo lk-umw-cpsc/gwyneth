05c9e6dedea7f6a15c181f74d82eddfd23554ad1
Matt Nguyen <92770341+mnnguyen4@users.noreply.github.com>
2023-03-08 14:15:30 -0500

    Fixed session issue
    There was an issue checking the access level where the if statement would send the user back to index.php. Fixed this issue by adding session functions.

--- event.php
+++ event.php
@@ -1,9 +1,13 @@
 <?php 
+
+  session_cache_expire(30);
+  session_start();
+
   // Ensure user is logged in
-  /*if (!isset($_SESSION['access_level']) || $_SESSION['access_level'] < 1) {
+  if (!isset($_SESSION['access_level']) || $_SESSION['access_level'] < 1) {
       header('Location: login.php');
       die();
-  }*/
+  }
 
 ?>
 
