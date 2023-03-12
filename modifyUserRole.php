3f5fa57611eac6d5eccab8d4bddbdbdf99efe15b
Tamra <tam4477@gmail.com>
2023-03-12 17:26:36 -0400

    Modified page First and Last Name to just Name; Updated some terms/titles used on page; Displayed Status for Active or Inactive w current status radio button checked; Added drop down Reasons for Status change.

--- modifyUserRole.php
+++ modifyUserRole.php
@@ -31,7 +31,7 @@
     <body>
         <?php require_once('header.php') ?>
         <main>
-            <h1>Changing User Access Level</h1>
+            <h1>Modify User Access</h1>
             <!-- Your code goes here. Be sure to wrap any form elements in a <form> tag -->
             <?php 
                 if ($_SERVER["REQUEST_METHOD"] == "GET") {
@@ -41,45 +41,65 @@
                     $thePerson = retrieve_person($id);   
                 }
             ?>
-            <form class="modUser" style="background-color:#E6E6FA">
+            <form class="modUser">
             <br>
             <label>
-                First Name: <?php echo $thePerson->get_first_name(); ?> 
+                Name:  <?php echo $thePerson->get_first_name() . " " . $thePerson->get_last_name(); ?> 
             </label>
             <br>
-            <label>
+            <!-- <label>
                 Last Name:  <?php echo $thePerson->get_last_name(); ?> 
             </label>
             <br>
+	    -->
             <label>
-                Select User: <?php echo implode(" ",$thePerson->get_type()); ?>
-            <!-- <select>  This drop down is redundent
-                <option></option>
-                <option value="admin">Admin</option>
-                <option value="superAdmin">Super Admin</option>
-                <option value="volunteer">Volunteer</option>
-            </select>
-            -->
+                Role:  <?php echo implode(" ",$thePerson->get_type()); ?>
+            </label>
+	    <br>
+	    <br>
+	    <label>
+ 
+	    <?php
+	    // Provides drop down of the role types to select, other than the current person's role type, to change the role
+		$roles = array('volunteer' => 'Volunteer', 'SuperAdmin' => 'SuperAdmin', 'Admin' => 'Admin');
+                echo '<p>Change Role:<select class="form-select-sm" name="s_type">' ;
+                echo '<option value="" SELECTED></option>' ;
+                foreach ($roles as $role => $typename) {
+			if($role != (implode(" ",$thePerson->get_type())))
+                        	echo '<option value="'.$role.'">'.$typename.'</option>';
+		}
+                echo '</select>';
+	    ?>
             </label>
             <br>
+	    <br>
             <label>
                 Status: 
             <br>
-            <?php 
-            // here I used a simple if else to check if the 'type' is volunteer then only show 'inactive' radio botton
-            // if it admin or super admin it will show the other radio bottons
-                if(implode(" ",$thePerson->get_type()) == "volunteer"){
-                    echo "<input type='radio' name='statsRadio' id = 'notActive' value='inactive'>";
-                    echo '<label for="notActive">Inactive</label><br>';
-
-                }else{
-                    echo '<input type="radio" name="statsRadio" id = "promo" value="promote">';
-                    echo '<label for="promo">Promote</label><br>';
-                    echo '<input type="radio" name="statsRadio" id = "promo" value="promote">';
-                    echo '<label for="demote">Demote</label><br>';
-                }
+            <?php
+	    // Check the person's status and check the radio to signal the current status
+	    // Display the current and other available statuses as well to change the status
+	    	$currentStatus = $thePerson->get_status();
+	    	if ($currentStatus == "Active") {
+                	echo '<input type="radio" name="statsRadio" id = "makeActive" value="Active" checked>';
+                	echo '<label for="makeActive">  Active&nbsp&nbsp&nbsp</label>';
+			echo '<input type="radio" name="statsRadio" id = "makeNotActive" value="Inactive">';
+                	echo '<label for="makeNotActive">  Inactive</label><br><br>';
+	    	} elseif ($currentStatus = "Inactive") {
+                	echo '<input type="radio" name="statsRadio" id = "makeActive" value="Active">';
+                	echo '<label for="makeActive">  Active&nbsp&nbsp&nbsp</label>';
+			echo '<input type="radio" name="statsRadio" id = "makeNotActive" value="Inactive" checked>';
+                	echo '<label for="makeNotActive">  Inactive</label><br><br>';
+	    	}
+	    	$reasons = array('Administrative', 'Volunteer Requested Status Change', 'Volunteer with 1 or more No Shows');
+	    	echo '<p>Reason:<select class="form-select-sm" name="s_type">';
+	    	echo '<option value="" SELECTED></option>';
+	    	foreach ($reasons as $reason)
+			echo '<option value='.$reason.'>'.$reason.'</option>';
+	    	echo '</select>';
             ?>
-            </label>
+            <br>
+	    </label>
             <br>
             <input type="submit">
             </form>
