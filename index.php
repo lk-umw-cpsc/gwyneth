ce6f7e1f77a028b23971c1ec4bcf3c3c14b931c0
Lauren Knight <lknight2@mail.umw.edu>
2023-03-22 10:18:45 -0400

    hide admin-only buttons from volunteers

--- index.php
+++ index.php
@@ -45,18 +45,20 @@
                     <img src="images/search.svg">
                     <span>Find Event</span>
                 </div>
-                <div class="dashboard-item" data-link="personSearch.php">
-                    <img src="images/person-search.svg">
-                    <span>Find Person</span>
-                </div>
-                <div class="dashboard-item" data-link="register.php">
-                    <img src="images/add-person.svg">
-                    <span>Register Volunteer</span>
-                </div>
-                <div class="dashboard-item" data-link="reports.php?venue=portland">
-                    <img src="images/create-report.svg">
-                    <span>Create Report</span>
-                </div>
+                <?php if ($_SESSION['access_level'] >= 2): ?>
+                    <div class="dashboard-item" data-link="personSearch.php">
+                        <img src="images/person-search.svg">
+                        <span>Find Person</span>
+                    </div>
+                    <div class="dashboard-item" data-link="register.php">
+                        <img src="images/add-person.svg">
+                        <span>Register Volunteer</span>
+                    </div>
+                    <div class="dashboard-item" data-link="reports.php?venue=portland">
+                        <img src="images/create-report.svg">
+                        <span>Create Report</span>
+                    </div>
+                <?php endif ?>
                 <div class="dashboard-item" data-link="editProfile.php">
                     <img src="images/manage-account.svg">
                     <span>Manage Profile</span>
