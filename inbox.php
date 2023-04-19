ac4b657e38d8698bdca21a5f068deab9b4e999fd
Lauren Knight <lknight2@mail.umw.edu>
2023-04-19 08:59:14 -0400

    no messages message added on empty inbox

--- inbox.php
+++ inbox.php
@@ -29,53 +29,59 @@
         <h1>Inbox</h1>
         <main class="general">
             <h2>Your Messages</h2>
-            <div class="table-wrapper">
-                <table class="general">
-                    <thead>
-                        <tr>
-                            <th style="width:1px">From</th>
-                            <th>Title</th>
-                            <th style="width:1px">Received</th>
-                        </tr>
-                    </thead>
-                    <tbody class="standout">
-                        <?php 
-                            require_once('database/dbMessages.php');
-                            require_once('database/dbPersons.php');
-                            require_once('include/output.php');
-                            $id_to_name_hash = [];
-                            foreach (get_user_messages($userID) as $message) {
-                                $sender = $message['senderID'];
-                                if (isset($id_to_name_hash[$sender])) {
-                                    $sender = $id_to_name_hash[$sender];
-                                } else {
-                                    $lookup = get_name_from_id($sender);
-                                    $id_to_name_hash[$sender] = $lookup;
-                                    $sender = $lookup;
+            <?php 
+                require_once('database/dbMessages.php');
+                $messages = get_user_messages($userID);
+                if (count($messages) > 0): ?>
+                <div class="table-wrapper">
+                    <table class="general">
+                        <thead>
+                            <tr>
+                                <th style="width:1px">From</th>
+                                <th>Title</th>
+                                <th style="width:1px">Received</th>
+                            </tr>
+                        </thead>
+                        <tbody class="standout">
+                            <?php 
+                                require_once('database/dbPersons.php');
+                                require_once('include/output.php');
+                                $id_to_name_hash = [];
+                                foreach ($messages as $message) {
+                                    $sender = $message['senderID'];
+                                    if (isset($id_to_name_hash[$sender])) {
+                                        $sender = $id_to_name_hash[$sender];
+                                    } else {
+                                        $lookup = get_name_from_id($sender);
+                                        $id_to_name_hash[$sender] = $lookup;
+                                        $sender = $lookup;
+                                    }
+                                    $messageID = $message['id'];
+                                    $title = htmlspecialchars($message['title']);
+                                    $timePacked = $message['time'];
+                                    $pieces = explode('-', $timePacked);
+                                    $year = $pieces[0];
+                                    $month = $pieces[1];
+                                    $day = $pieces[2];
+                                    $time = time24hto12h($pieces[3]);
+                                    $class = 'message';
+                                    if (!$message['wasRead']) {
+                                        $class .= ' unread';
+                                    }
+                                    echo "
+                                        <tr class='$class' data-message-id='$messageID'>
+                                            <td>$sender</td>
+                                            <td>$title</td>
+                                            <td>$month/$day/$year $time</td>
+                                        </tr>";
                                 }
-                                $messageID = $message['id'];
-                                $title = htmlspecialchars($message['title']);
-                                $timePacked = $message['time'];
-                                $pieces = explode('-', $timePacked);
-                                $year = $pieces[0];
-                                $month = $pieces[1];
-                                $day = $pieces[2];
-                                $time = time24hto12h($pieces[3]);
-                                $class = 'message';
-                                if (!$message['wasRead']) {
-                                    $class .= ' unread';
-                                }
-                                echo "
-                                    <tr class='$class' data-message-id='$messageID'>
-                                        <td>$sender</td>
-                                        <td>$title</td>
-                                        <td>$month/$day/$year $time</td>
-                                    </tr>";
-                            }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            <?php else: ?>
+                <p class="no-messages standout">You currently have no unread messages.</p>
+            <?php endif ?>
             <button>Compose New Message</button>
             <a class="button cancel" href="index.php">Return to Dashboard</a>
         </main>
