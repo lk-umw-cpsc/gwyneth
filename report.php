93547dbde3e0aaa087cd36db9f8fcb4c9cc9b45f
Lauren Knight <lknight2@mail.umw.edu>
2023-04-21 20:57:47 -0400

    add temporary workaround for indiv. hours report

--- report.php
+++ report.php
@@ -114,7 +114,7 @@
                                          <td>' . $person->get_first_name() . '</td>
                                          <td>' . $person->get_last_name() . '</td>
  					 <td><a href="mailto:' . $person->get_id() . '">' . $person->get_id() . '</a></td>
-				     <td><a href="reportsPage.php?report_type='. $report .'&date_from='. $dFrom .'&date_to='. $dTo .'&lname_start='. $lastFrom .'&lname_end='. $lastTo .'&name='. $name .'&indivID='. $person->get_id() .'">Run Report</a></td>
+				     <td><a href="volunteerReport.php?id=' . $person->get_id() .'">Run Report</a></td>
 				     </tr>';
                             }
                             echo '
